8abb9c7b80d84ccdd6435095156f67029fea8fa7
thanhhoa0000 <hoathanhnguyen.work@gmail.com>
2025-03-14 18:13:11 +0700

    chore: Fix some HTML and JS bugs

--- src/Client.Customers.Application/Dtos/CategoryDto.cs
+++ src/Client.Customers.Application/Dtos/CategoryDto.cs
@@ -4,10 +4,8 @@ public class CategoryDto
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     [Required, MinLength(2), MaxLength(50)]
-    public string? Name { get; set; }
+    public required string Name { get; set; }
     [Required, MinLength(2), MaxLength(50)]
-    public string? CodeName { get; set; }
+    public required string CodeName { get; set; }
     public Guid ConcurrencyStamp  { get; set; }
-    
-    public ICollection<StoreDto> Stores { get; set; } = new List<StoreDto>();
 }
