7bc5978c8455763f4ee949ef4a305f3a58b4738d
thanhhoa0000 <hoathanhnguyen.work@gmail.com>
2025-04-11 13:49:59 +0700

    feat: Build the create order fucntion

--- src/Client.Customers.Presentation/wwwroot/js/stores.js
+++ src/Client.Customers.Presentation/wwwroot/js/stores.js
@@ -205,7 +205,37 @@ function getSubCategoriesListForStorePage(categoryName) {
     });
 }
 
+function getItemsForStorePage(province, districts, category, subcategories) {
+    const path = window.location.pathname;
+    $.ajax({
+        url: `${path}?province=${province}&categoryName=${category}&districtsString=${districts}&subcategoriesString=${subcategories}`,
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        success: function (response) {
+            let tempDom = $('<div></div>').html(response);
+            let newStoresSection = tempDom.find('.store-main-content').html();
+            $('.store-main-content').html(newStoresSection);
+            $('.store-section-options p').html(tempDom.find('.store-section-options p').html());
 
+            if (subcategories.length > 0) {
+                setTimeout(function () {
+                    $('.store-category-item input').each(function () {
+                        if (subcategories.includes(this.id)) {
+                            $(this).prop('checked', true);
+
+                            let filterTag = document.querySelector('.store-section-category-filter-tag');
+                            let checkboxes = document.querySelectorAll('.store-category-item input');
+                            let checkedCount = Array.from(checkboxes).filter(checkbox => checkbox.checked).length;
+
+                            filterTag.style.display = 'flex';
+                            filterTag.querySelector('span').textContent = `(${checkedCount})`;
+                        }
+                    });
+                }, 100);
+            }
+        }
+    });
+}
 
 function initializeFilterTag(checkboxes, filterTag) {
     checkboxes.forEach(checkbox => {
