cd93e06f3fae1d34cb196609a3e0fb5dec1423e1
thanhhoa0000 <hoathanhnguyen.work@gmail.com>
2025-04-22 18:09:19 +0700

    chore: Fix minor bugs

--- src/Client.Customers.Application/Models/Dtos/StoreDto.cs
+++ src/Client.Customers.Application/Models/Dtos/StoreDto.cs
@@ -3,23 +3,28 @@
 public class StoreDto
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    [Required]
-    public Guid UserId { get; set; }
-    [MaxLength(20)]
-    public string? WardCode { get; set; }
-    [MaxLength(100)]
-    public string? StreetName { get; set; }
-    [Required, MinLength(10), MaxLength(50)]
-    public string? Name { get; set; }
-    [Required]
-    public TimeOnly OpeningHour { get; set; }
-    [Required]
-    public TimeOnly ClosingHour { get; set; }
-    public string? CoverImagePath { get; set; }
-    public double Rating { get; set; } = 0.0;
-    public int Sold { get; set; } = 0;
-    public bool IsPromoted { get; set; } = false;
-    
-    public WardDto? Ward { get; set; }
-    public ICollection<SubCategoryDto> SubCategories { get; set; } = new List<SubCategoryDto>();
+        [Required]
+        public Guid UserId { get; set; }
+        [MaxLength(20)]
+        public string? WardCode { get; set; }
+        [MaxLength(100)]
+        public string? StreetName { get; set; }
+        [Required, MinLength(10), MaxLength(50)]
+        public string? Name { get; set; }
+        [Required]
+        public TimeOnly OpeningHour { get; set; }
+        [Required]
+        public TimeOnly ClosingHour { get; set; }
+        public string? CoverImagePath { get; set; }
+        public double Rating { get; set; } = 0.0;
+        public int Sold { get; set; } = 0;
+        public bool IsPromoted { get; set; } = false;
+        public StoreState State { get; set; } = StoreState.Active;
+        public Guid ConcurrencyStamp  { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? LastUpdatedAt { get; set; }
+        
+        public WardDto? Ward { get; set; }
+        public ICollection<SubCategoryDto> SubCategories { get; set; } = new List<SubCategoryDto>();
+        public ICollection<CollectionDto> Collections { get; set; } = new List<CollectionDto>();
 }
